f2338f642032837031466842c25345dd70d4b4d0
Jacob Brown <Jacob_E_Brown@protonmail.com>
2024-09-25 18:20:29 -0600

    Create new project Folder. Created a new animated SVG image for the Main page.

---
+++ SWDV 102/SWDV 140/Project 3/scripts/contact.js
@@ -0,0 +1,29 @@
+"use strict";
+
+let form = document.querySelector('#contact-form');
+const submitButton = document.querySelector('#submit-button');
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    let name = document.querySelector('#name');
+    let email = document.querySelector('#email');
+    let subject = document.querySelector('#subject');
+    let message = document.querySelector('#message');
+
+    if (name.value.trim() === "") {
+        alert("Please Enter A Name!");
+    } else if (email.value.trim() === "") {
+        alert("Please Enter An Email!");
+    } else if (subject.value.trim() === "") {
+        alert("Please Enter Subject!");
+    } else if (message.value.trim() === "") {
+        alert("Please Enter A Message!");
+    } else {
+        alert("This form has been successfully submitted! \nThank you!");
+        name.value = "";
+        email.value = "";
+        subject.value = "";
+        message.value = "";
+    }
+});
---
+++ SWDV 102/SWDV 140/Project 3/scripts/navScript.js
@@ -0,0 +1,30 @@
+// 9-1-24
+// Jacob Brown
+
+// This script is meant to show and hide the menu when the menu icon element
+// is pressed when the screen size is under 426px.
+// The menu is always present but invisible.
+// When the screen is larger than 426px, 
+// the menu disapears and the naviagation menu changes to be a horizontal
+// always visible element.
+
+"use strict";
+
+document.getElementById('menu-button').onclick = showHideNavigation;
+
+function showHideNavigation() {
+    // alert("button was pressed")
+
+    // Creating a variable to hold the navigation object by means of its id
+    let navigationMenu = document.getElementById("primary-header__navigation");
+    // Getting the current status of the opacity levels
+    let menuOpenVariable = window.getComputedStyle(navigationMenu).opacity;
+
+    // If the opacity of the menu is 0, then the opacity is change to one. Else it is reversed.
+    // This is 
+    if (menuOpenVariable == "0") {
+        navigationMenu.style.opacity = "1";
+    } else {
+        navigationMenu.style.opacity = "0";
+    }
+}
