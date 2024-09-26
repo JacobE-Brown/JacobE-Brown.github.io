6f2c63de71ef7998050df69e73a203d7b06f3efb
Jacob Brown <Jacob_E_Brown@protonmail.com>
2024-09-26 16:16:54 -0600

    Created the basline CSS and HTML

---
+++ SWDV 102/PairPrograming/Game-Website/scripts/tic-tac-toe.js
@@ -0,0 +1,23 @@
+"use strict";
+window.addEventListener('DOMContentLoaded', () => {
+    /*
+    Indexes within the board
+    [0] [1] [2]
+    [3] [4] [5]
+    [6] [7] [8]
+ */
+    const winningConditions = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+    const tttboxes = document.querySelectorAll('[title="TTT-Box"]');
+    const handleClick = (event) => {
+        console.log('TTT-Box clicked!', event);
+    };
+});
