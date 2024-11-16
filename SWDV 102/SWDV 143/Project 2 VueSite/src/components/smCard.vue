bb6c4c5cef815b305d4416f7525e1a7eee2cce08
Jacob Brown <Jacob_E_Brown@protonmail.com>
2024-11-16 12:46:22 -0700

    Created The Vue Site with NPM, installed PureCSS, Implemented a basic Grid System, Implemented a basic social media card side element on the Home Page.

---
+++ SWDV 102/SWDV 143/Project 2 VueSite/src/components/smCard.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+    name: 'MyComponent',
+};
+</script>
+
+<template>
+    <div class="pure-g">
+        <div class="pure-u-sm-1 pure-u-md-1-3">
+            <h3>
+                <slot name="post-title">Slot Terms</slot>
+            </h3>
+            <slot name="post-link">
+                <a href="#" class="pure-button">
+                    <slot name="post-button">Button</slot>
+                </a>
+            </slot>
+
+        </div>
+        <div class="pure-u-sm-1 pure-u-md-2-3">
+            <slot name="post-image"><img src="https://picsum.photos/300/100"></slot>
+            <p>
+                <slot name="post-content"> Ea ex blanditiis recusandae debitis tenetur. Voluptates aut reprehenderit.
+                </slot>
+            </p>
+        </div>
+    </div>
+</template>
+
+<style scoped></style>
---
+++ SWDV 102/SWDV 143/Project 2 VueSite/src/main.js
@@ -0,0 +1,7 @@
+// Added 11-14 and adds PureCSS to the app
+import 'purecss/build/pure-min.css';
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
